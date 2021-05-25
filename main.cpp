a0c52c2d93f1ad4bf3ba55bf27f4f73ff6968145
Nirbhay-Gaur <nirbhaygaur.cer18@itbhu.ac.in>
2021-05-25 15:52:57 +0530

    implement great circle distance formula

--- main.cpp
+++ main.cpp
@@ -8,3 +8,16 @@
 
 // If distance is found to be less than or equal to 50kms then output the user-id and name of the cab driver to a new file else take no action.
 
+#include <bits/stdc++.h>
+using namespace std;
+
+// latitude and longitude of the customer who needs a cab
+#define lat1d 12.9611159
+#define lon1d 77.6362214
+
+// values of pi and earth radius
+#define pi 3.14159265358979323846
+#define earth_radius 6371.0
+
+ifstream customer_list ("customer.json");
+ofstream out ("answer.json");
