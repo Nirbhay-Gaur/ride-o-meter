1542a1eda800a8a5f6c71e8ad2ca429e1ddbe0f8
Nirbhay-Gaur <nirbhaygaur.cer18@itbhu.ac.in>
2021-05-25 15:28:59 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+// Obtain latitude and longitude of each cab in string format along with their user-id and name from JSON encoded input file.
+
+// Convert latitude and longitude of the cab present in string format to double.
+
+// Convert latitude and longitude of both user and cab present in degrees to radians.
+
+// Calculate distance between the user's location and the cab using Great Circle Distance formula.
+
+// If distance is found to be less than or equal to 50kms then output the user-id and name of the cab driver to a new file else take no action.
+
